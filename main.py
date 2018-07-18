593e7321e3d0b0fb99356f05920c3b26c5f08ff7
ilais <ilai.shai6@gmail.com>
2018-07-17 17:39:13 -0700

    edited movment, added score and tail for snake

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import window
 import player
+import time
 
 pygame.init()
 screen = pygame.display.set_mode((16 * 50, 16 * 50))
@@ -20,13 +21,26 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                p1.yloc -= 1
+                #p1.yloc -= 1
+                p1.face = "up"
             if event.key == pygame.K_a:
-                p1.xloc -= 1
+                #p1.xloc -= 1
+                p1.face = "lft"
             if event.key == pygame.K_s:
-                p1.yloc += 1
+                #p1.yloc += 1
+                p1.face = "dn"
             if event.key == pygame.K_d:
-                p1.xloc += 1
+                #p1.xloc += 1
+                p1.face = "rt"
+    time.sleep(0.05)
+    if p1.face == "up":
+        p1.yloc -= 1
+    if p1.face == "lft":
+        p1.xloc -= 1
+    if p1.face == "dn":
+        p1.yloc += 1
+    if p1.face == "rt":
+        p1.xloc += 1
 
     p1.drawplayer()
     distance = ((win.grid.objective[0] - p1.xloc)**2 + (win.grid.objective[1] - p1.yloc)**2)**0.5
--- player.py
+++ player.py
@@ -3,15 +3,19 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self, xloc=40, yloc=30, score=0):
+    def __init__(self, xloc=40, yloc=30, score=0, tail=0, face=""):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((800, 800), pygame.SRCALPHA)
         #self.image.fill()
         self.xloc = xloc
         self.yloc = yloc
         self.score = score
+        self.tail = tail
+        self.face = face
 
     def drawplayer(self):
         self.image.fill(pygame.SRCALPHA)
-        pygame.draw.circle(self.image, (0, 0, 255), (self.xloc * 10, self.yloc * 10), 10)
+        #pygame.draw.circle(self.image, (0, 0, 255), (self.xloc * 10, self.yloc * 10), 10)
+        player = pygame.rect.Rect(self.xloc * 10, self.yloc * 10, 10, 10)
+        pygame.draw.rect(self.image, (0, 0, 255), player)
         return self.image
